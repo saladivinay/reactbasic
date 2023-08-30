37f6bbf88a053dbf800e4d3989835d041f75e4a3
saladivinay <saladivinay333@gmail.com>
2023-08-30 19:50:25 +0530

    first

--- src/Todolist.jsx
+++ src/Todolist.jsx
@@ -4,6 +4,9 @@ function Todolist(props){
     return(
         <div className='box'>
             <h1>{props.title}</h1>
+            <h4>Captain:{props.head}</h4>
+            <img src={props.im}/>
+            <h4><u>team</u></h4>
             <ul>
                 {
                     todo.map((to)=>{
