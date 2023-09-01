b981de81f592170d4c9bccc3daaffa95381b73d9
saladivinay <saladivinay333@gmail.com>
2023-09-01 20:10:14 +0530

    bn

---
+++ src/Todolist.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+function Todolist(props){
+    var [todo,setTodo]=React.useState([...props.ar])
+    return(
+        <div className='box'>
+            <h1>{props.title}</h1>
+            <h4>Captain:{props.head}</h4>
+            <img src={props.im.src}/>
+            <h4><u>team</u></h4>
+            <ul>
+                {
+                    todo.map((to)=>{
+                        return <li>{to}</li>
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
+export default Todolist;
